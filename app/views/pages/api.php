3905337fed91d4570fc9b7ed876e6e4662f8e89a
Dmitry Kuleznev <screamninja@gmail.com>
2018-11-01 01:11:43 +0300

    about API

--- app/views/pages/api.php
+++ app/views/pages/api.php
@@ -2,7 +2,16 @@
     <section>
         <h3>API Request Example</h3>
         <pre>
-{"jsonrpc": "2.0", "method": "getJson", "params": {"count": 2}, "id": 1}
+METHOD: POST
+URL: http://php.fw/api
+HEADERS:
+     KEY: x-authorization-token
+     VALUE: Your API Token, example - $2y$10$VUc2LGIKLWrvCeGPEBEDBucz/PZBjg89RbK.976D4.0feverU.u2K (<-- Not working)
+
+     KEY: Content-Type
+     VALUE: application/json
+
+BODY: {"jsonrpc": "2.0", "method": "getJson", "params": {"count": 2}, "id": 1}
     </pre>
         <h3>API Response Example</h3>
         <pre>
